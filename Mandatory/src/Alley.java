cc6dd89cf697e94bf1ce2adc12723a7889849170
Oliver Haagh <ohaagh@gmail.com>
2015-10-22 13:47:30 +0200

    Ændret måden hvorpå vi bruger semaphores, så vi kun behøver en semaphore
    for hver retning, så vi ikke behøver en for hver bil.

--- Mandatory/src/Alley.java
+++ Mandatory/src/Alley.java
@@ -1,46 +1,50 @@
 
 class Alley {
 	
-	private Semaphore[] cars = new Semaphore[8];
-	
+	private Semaphore up, down = new Semaphore(1);
+	private boolean passing;
 	private int inAlley = 0;
 	
 	public void enter(int no) throws InterruptedException {
-		cars[no].P();
-		if (inAlley++ == 0) {
 			switch(no) {
 			case 1:
 			case 2:
 			case 3:
 			case 4:
-				for (int i = 4; i < 8; i++) {
-					cars[i].P();
+				up.P();
+				if (!passing) {
+					passing = true;
 				}
+				inAlley++;
+				down.P();
+				up.V();
 			default:
-				for (int i = 0; i < 4; i++) {
-					cars[i].P();
+				down.P();
+				if (!passing) {
+					passing = true;
 				}
+				inAlley++;
+				up.P();
+				down.V();
 			}
-		}
 	}
 	
 	public void leave(int no) {
-		cars[no].V();
-		if (--inAlley == 0) {
 			switch(no) {
 			case 1:
 			case 2:
 			case 3:
 			case 4:
-				for (int i = 4; i < 8; i++) {
-					cars[i].V();
+				if (--inAlley == 0) {
+					passing = false;
+					down.V();
 				}
 			default:
-				for (int i = 0; i < 4; i++) {
-					cars[i].V();
+				if (--inAlley == 0) {
+					passing = false;
+					up.V();
 				}
 			}
-		}
 	}
 
 }
