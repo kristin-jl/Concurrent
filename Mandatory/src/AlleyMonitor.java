7fb36e381880074cffccbace9db2f12d55ef4d5e
Kristin <k.lillekjendlie@gmail.com>
2015-11-08 13:02:54 +0100

    Glemte at pushe ^^

---
+++ Mandatory/src/AlleyMonitor.java
@@ -0,0 +1,35 @@
+// Copy into CarContol, line 163
+
+
+               
+               
+ // Code from here
+
+
+class AlleyMonitor {
+	private int up, down;
+	
+	public synchronized void enterUp() throws InterruptedException {
+		while(down > 0) {
+			wait();
+		}
+		up++;
+	}
+	
+	public synchronized void enterDown() throws InterruptedException {
+		while(up > 0) {
+			wait();
+		}
+		down++;
+	}
+	
+	public synchronized void leaveUp() {
+		up--;
+		notifyAll();
+	}
+	
+	public synchronized void leaveDown() {
+		down--;
+		notifyAll();
+	}
+}
