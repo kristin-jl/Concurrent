6cfe5e86d026c0aacb991268142da736e50a83f4
Oliver Haagh <ohaagh@gmail.com>
2015-11-12 15:09:05 +0100

    Fair alley virker stadig ikke

--- Mandatory/src/AlleyMonitor.java
+++ Mandatory/src/AlleyMonitor.java
@@ -1,18 +1,25 @@
 class AlleyMonitor {
 	private int up, down, activeUp, activeDown, enteredUp, enteredDown;
+	private boolean locked;
 	
 	public synchronized void enterUp() throws InterruptedException {
 		while(down > 0) {
 			wait();
 		}
-		if (activeUp > enteredUp && down == 0) {
-			enteredUp++;
-			
-		} else {
-			enteredUp = 0;
-			while(enteredDown <= 4 && enteredDown > 0) {
+		if (activeUp > enteredUp) {
+			while(locked) {
 				wait();
 			}
+			enteredUp++;
+			notifyAll();
+		} else {
+			enteredUp = 0;
+			locked = true;
+			notifyAll();
+			while(enteredDown > 0 && locked) {
+				wait();
+			}
+			enteredUp++;
 		}
 		up++;
 	}
@@ -21,16 +28,20 @@ class AlleyMonitor {
 		while(up > 0) {
 			wait();
 		}
-		if (activeDown > enteredDown && up == 0) {
+		if (activeDown > enteredDown) {
 			enteredDown++;
+			notifyAll();
 		} else {
 			enteredDown = 0;
-			while (enteredUp <= 4 && enteredUp > 0) {
+			locked = false;
+			notifyAll();
+			while (enteredUp > 0) {
 				wait();
 			}
+			enteredDown++;
 		}
 		down++;
-	}
+	} 
 	
 	public synchronized void leaveUp() {
 		up--;
