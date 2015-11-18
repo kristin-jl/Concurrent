060144034454bc3f534257debd5f3ccd220f6307
Oliver Haagh <ohaagh@gmail.com>
2015-11-18 12:21:36 +0100

    Barrier virkede selvom den ikke var aktiv

--- Mandatory/src/Barrier.java
+++ Mandatory/src/Barrier.java
@@ -8,39 +8,41 @@ class Barrier {
 	
 	// solution is from The Little Book of Semaphores pages 39-41
 	public void sync() throws InterruptedException {
-		mutex.P();
-		count++;
-		if (count == n) { // if true, let cars that are waiting at the line proceed
-			turnstile2.P();
-			turnstile.V();
-		}
-		mutex.V();
-			
-		// wait if not all cars have arrived
-		turnstile.P();
-		turnstile.V();
-		// if the barrier is not active at this point release all cars that have been blocked
-		if (!active) {
-			return;
-		}
+		if(active) {
+			mutex.P();
+			count++;
+			if (count == n) { // if true, let cars that are waiting at the line proceed
+				turnstile2.P();
+				turnstile.V();
+			}
+			mutex.V();
 				
-		mutex.P();
-		count--;
-		if (count == 0) { // if true, let all cars pass the line
+			// wait if not all cars have arrived
 			turnstile.P();
+			turnstile.V();
+			// if the barrier is not active at this point release all cars that have been blocked
+			if (!active) {
+				return;
+			}
+					
+			mutex.P();
+			count--;
+			if (count == 0) { // if true, let all cars pass the line
+				turnstile.P();
+				turnstile2.V();
+			}
+			mutex.V();
+				
+			// wait for all cars to be ready to pass the line
+			turnstile2.P();
 			turnstile2.V();
+			mutex.P();
+			// if car0 has been deactivated at this point, make sure that n is set to 8
+			if (!car0Active && n == 9) {
+				n = 8;
+			}
+			mutex.V();
 		}
-		mutex.V();
-			
-		// wait for all cars to be ready to pass the line
-		turnstile2.P();
-		turnstile2.V();
-		mutex.P();
-		// if car0 has been deactivated at this point, make sure that n is set to 8
-		if (!car0Active && n == 9) {
-			n = 8;
-		}
-		mutex.V();
 	}
 		
 	
